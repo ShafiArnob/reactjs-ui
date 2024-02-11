810bee06893c0f2808af251a6ac1e61c467b1cab
ShafiArnob <shafiarnob@gmail.com>
2024-02-11 16:25:41 +0600

    fix-ui: card alignments and typograpy

--- src/components/UserCard.tsx
+++ src/components/UserCard.tsx
@@ -1,5 +1,6 @@
 import { User } from "@/types/user";
 import { Link } from "react-router-dom";
+import { Card, CardContent, CardHeader } from "./ui/card";
 
 interface UserCardProps {
   user: User;
@@ -7,19 +8,23 @@ interface UserCardProps {
 
 const UserCard: React.FC<UserCardProps> = ({ user }) => {
   return (
-    <div className="rounded-2xl h-[440px] max-w-[340px] flex flex-col justify-center items-start p-4 overflow-hidden shadow-md border border-gray-50  hover:scale-[101%] transition-all gap-1">
-      <Link to={`/${user.id}`} className="w-full">
-        <img src={user?.image} alt="user" className="mb-1" />
-        <h1 className="text-xl font-semibold">
-          {user?.firstName} {user.lastName}
-        </h1>
-      </Link>
-      <p>Email: {user.email}</p>
-      <p className="text-sm">
-        Address: {user.address.address}, {user.address.city}
-      </p>
-      <p className="text-sm">Company name: {user.company.name}</p>
-    </div>
+    <Card className="h-[440px] max-w-[340px] border-gray-50 hover:scale-[101%] transition-all shadow-sm">
+      <CardHeader className="mb-1 pb-0">
+        <Link to={`/${user?.id}`} className="w-full">
+          <img src={user?.image} alt="user" className="mb-1" />
+          <h1 className="text-xl font-semibold">
+            {user?.firstName} {user?.lastName}
+          </h1>
+        </Link>
+      </CardHeader>
+      <CardContent className="text-zinc-800">
+        <p>Email: {user?.email}</p>
+        <p className="text-sm">
+          Address: {user.address.address}, {user?.address?.city}
+        </p>
+        <p className="text-sm">Company name: {user?.company?.name}</p>
+      </CardContent>
+    </Card>
   );
 };
 
