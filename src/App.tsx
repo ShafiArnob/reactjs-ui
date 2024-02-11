173baa4cdd48ca338afec294c6de4f2aa2774f80
ShafiArnob <shafiarnob@gmail.com>
2024-02-11 14:23:03 +0600

    init shadcn ui library

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,12 @@
 import { useState } from "react";
+import { Button } from "@/components/ui/button";
 
 function App() {
   const [count, setCount] = useState(0);
 
   return (
     <>
-      <h1>Hello World</h1>
+      <Button>Hello World</Button>
     </>
   );
 }
