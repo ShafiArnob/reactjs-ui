10a8e4beb6a7646eb925744c294e96351bfd8ad4
ShafiArnob <shafiarnob@gmail.com>
2024-02-11 18:12:04 +0600

    user page responsive

--- src/pages/UserPage.tsx
+++ src/pages/UserPage.tsx
@@ -26,7 +26,7 @@ const UserPage = () => {
   return (
     <Container>
       <div className="flex flex-col h-screen w-full items-center mt-8">
-        <div className="p-2 w-2/3">
+        <div className="p-2 w-10/12 md:2/3">
           <div className="w-[70px]">
             <Link
               to="/"
@@ -37,37 +37,37 @@ const UserPage = () => {
             </Link>
           </div>
         </div>
-        <div className="flex border-2 rounded-2xl p-2 w-2/3 gap-4 border-purple-300">
-          <div className="bg-purple-100 rounded-2xl p-2">
+        <div className="grid grid-cols-1 sm:grid-cols-1 md:grid-cols-3 border-2 rounded-2xl p-2 w-10/12 md:2/3 gap-4 border-purple-300">
+          <div className="flex justify-center bg-purple-100 rounded-2xl p-2">
             <img src={user?.image} alt="user" className="mb-1" />
           </div>
-          <div className="space-y-1 p-2 my-2">
+          <div className="space-y-1 p-2 my-2 md:col-span-2 ">
             {/* <h1 className="text-xl font-semibold">
               {user?.firstName} {user?.lastName}
             </h1> */}
-            <h2 className="text-xl text-zinc-600">
+            <h2 className="text-lg md:text-xl text-zinc-600">
               First name:{" "}
               <span className="text-zinc-700 font-semibold">
                 {user?.firstName}
               </span>
             </h2>
-            <h2 className="text-xl text-zinc-600">
+            <h2 className="text-lg md:text-xl text-zinc-600">
               Last name:{" "}
               <span className="text-zinc-700 font-semibold">
                 {user?.lastName}
               </span>
             </h2>
-            <h3 className="text-xl text-zinc-600">
+            <h3 className="text-lg md:text-xl text-zinc-600">
               Email:{" "}
               <span className="text-zinc-700 font-semibold">{user?.email}</span>
             </h3>
-            <h3 className="text-xl text-zinc-600">
+            <h3 className="text-lg md:text-xl text-zinc-600">
               Address:{" "}
               <span className="text-zinc-700 font-semibold">
                 {user?.address?.address}, {user?.address?.city}
               </span>
             </h3>
-            <h3 className="text-xl text-zinc-600">
+            <h3 className="text-lg md:text-xl text-zinc-600">
               Company name:{" "}
               <span className="text-zinc-700 font-semibold">
                 {user?.company?.name}
