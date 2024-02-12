79be9ab1c4f8a8cdafefc3516fcf4ad3381b2a57
ShafiArnob <shafiarnob@gmail.com>
2024-02-12 11:31:24 +0600

    add new user form

--- src/components/AddUserBtn.tsx
+++ src/components/AddUserBtn.tsx
@@ -1,10 +1,16 @@
 import { Button } from "./ui/button";
 import { FaPlus } from "react-icons/fa";
-const AddUserBtn = () => {
+
+interface AddUserBtnProps {
+  setShowUserForm: React.Dispatch<React.SetStateAction<boolean>>;
+}
+
+const AddUserBtn: React.FC<AddUserBtnProps> = ({ setShowUserForm }) => {
   return (
     <Button
       className="bg-purple-500 hover:bg-purple-600 font-semibold h-[39px] space-x-0.5"
       size="sm"
+      onClick={() => setShowUserForm((prev) => !prev)}
     >
       <FaPlus />
       <span>Add user</span>
--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -1,5 +1,6 @@
 import { getAllUsers } from "@/api/user";
 import AddUserBtn from "@/components/AddUserBtn";
+import AddUserForm from "@/components/AddUserForm";
 import Container from "@/components/Container";
 import SearchBar from "@/components/SearchBar";
 import { SortUsersSelect } from "@/components/SortUsersSelect";
@@ -11,6 +12,7 @@ const Home = () => {
   const [users, setUsers] = useState<User[]>([]);
   const [searchInput, setSearchInput] = useState("");
   const [sortSelectInput, setSortSelectInput] = useState("name");
+  const [showUserForm, setShowUserForm] = useState(false);
 
   useEffect(() => {
     const fetchUsers = async () => {
@@ -23,10 +25,13 @@ const Home = () => {
 
   return (
     <Container>
-      <div className="flex justify-center my-4 p-4 space-x-3">
+      <div className="flex justify-center my-1 mt-2 p-4 space-x-3 ">
         <SearchBar setSearchInput={setSearchInput} />
         <SortUsersSelect setSortSelectInput={setSortSelectInput} />
-        <AddUserBtn />
+        <AddUserBtn setShowUserForm={setShowUserForm} />
+      </div>
+      <div className="my-2 p-4 transition-all duration-500 ease-in-out">
+        {showUserForm && <AddUserForm />}
       </div>
       <div className="grid grid-cols-1 sm:grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-6 p-4 ">
         {users
